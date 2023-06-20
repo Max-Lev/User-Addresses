7e6fa4374bc23d85edf34854fef0ec5183ec57bb
Max-Lev <maxlevtov@gmail.com>
2023-06-20 16:39:54 +0300

    cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const app = express()
 const port = 3000;
 const jsonMiddleware = express.json();
 
-app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
 // app.use(cors({
 //   origin: 'https://user-addresses.vercel.app/',
 //   optionsSuccessStatus: 200
