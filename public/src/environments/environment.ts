ee695c4ca9716a90ee0a299deffd999f44882c94
Max-Lev <maxlevtov@gmail.com>
2025-01-20 13:25:51 +0200

    Update environment.ts

--- public/src/environments/environment.ts
+++ public/src/environments/environment.ts
@@ -5,12 +5,12 @@ export const environment = {
     // 'https://us-central1-addresses-f230c.cloudfunctions.net/'
     api: 'https://us-central1-addresses-f230c.cloudfunctions.net/api',
     firebaseConfig: {
-        apiKey: "AIzaSyCLD5WCuAb5NG7_ToefSLk98PTZBibXaKA",
-        authDomain: "addresses-f230c.firebaseapp.com",
-        projectId: "addresses-f230c",
-        storageBucket: "addresses-f230c.appspot.com",
-        messagingSenderId: "597892672977",
-        appId: "1:597892672977:web:177e4fd1e855005b8184f0",
-        measurementId: "G-Q1MHJPDQ16"
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
     }
 };
